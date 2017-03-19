a028ef88e5ca921488ee998980a5b5d419e109ef
eleven18 <piotrekr248@gmail.com>
2017-03-19 17:21:13 +0100

    FollowCamera
    Podazanie kamery za graczem

---
+++ FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    private Transform m_CameraTransform;
+    public Transform m_PlayerTransform;
+
+	void Start ()
+    {
+        m_CameraTransform = GetComponent<Transform>();
+	}
+	
+
+	void Update ()
+    {
+        if(m_PlayerTransform.position.x <= 0)
+        {
+            m_CameraTransform.position = new Vector3(0, 0, -10);
+        }
+        else
+        {
+            m_CameraTransform.position = new Vector3(m_PlayerTransform.position.x, 0, -10);
+        }
+
+        if(m_PlayerTransform.position.x >= 158)
+        {
+            m_CameraTransform.position = new Vector3(158, 0, -10);
+        }
+        
+	}
+}
