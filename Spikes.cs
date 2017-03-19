a9f49c91b6832b676d533950c9d621c84f6c428b
eleven18 <piotrekr248@gmail.com>
2017-03-19 17:21:42 +0100

    Spikes
    Poczatek skryptu dla kolcow

---
+++ Spikes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+   
+
+    private Rigidbody2D m_Rigidbody;
+    private float m_Speed= 160;
+
+	void Start ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            m_Rigidbody.velocity = new Vector2(m_Speed * Time.deltaTime, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
